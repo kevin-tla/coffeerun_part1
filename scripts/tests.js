c0e9df00fb399afd8f9acea8e0dc8f5d78a1f598
vertepenguin <kevintlam9653@yahoo.com>
2017-03-06 21:36:30 -0800

    Add files via upload

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,12 +1,59 @@
-QUnit.test("hello test", function(assert) {
-    assert.ok(1 == "1", "Passed!");
+QUnit.test('8.10', function(assert) {
+    var ds = new App.DataStore();
+    ds.add('m@bond.com', 'tea');
+    ds.add('james@bond.com', 'eshpressho');
+    assert.deepEqual(ds.getAll(), {
+        'm@bond.com': 'tea',
+        'james@bond.com': 'eshpressho'
+    });
 
-    (function(window) {
-        'use strict';
-        var App = window.App;
-        var Truck = App.Truck;
-        var DataStore = App.DataStore;
-        var myTruck = new Truck('ncc-1701', new DataStore());
-        window.myTruck = myTruck;
-    })(window);
+    ds.remove('james@bond.com');
+    assert.deepEqual(ds.getAll(), {
+        'm@bond.com': 'tea'
+    });
+    assert.equal(ds.get('m@bond.com'), 'tea');
+    assert.equal(ds.get('james@bond.com'), undefined);
+});
+
+QUnit.test('8.32', function(assert) {
+    var db = new App.DataStore();
+    var myTruck = new App.Truck('7', db);
+
+    myTruck.createOrder({
+        emailAddress: 'me@goldfinder.com',
+        coffee: 'double mocha'
+    });
+    myTruck.createOrder({
+        emailAddress: 'dr@no.com',
+        coffee: 'decaf'
+    });
+    myTruck.createOrder({
+        emailAddress: 'm@bond.com',
+        coffee: 'earl grey'
+    });
+
+    myTruck.printOrders();
+    assert.deepEqual(myTruck.getAllOrders(), {
+        'me@goldfinder.com': {
+            'coffee': 'double mocha',
+            'emailAddress': 'me@goldfinder.com'
+        },
+        'dr@no.com': {
+            'coffee': 'decaf',
+            'emailAddress': 'dr@no.com'
+        },
+        'm@bond.com': {
+            'coffee': 'earl grey',
+            'emailAddress': 'm@bond.com'
+        }
+    });
+    myTruck.deliverOrder('dr@no.com');
+    myTruck.deliverOrder('m@bond.com');
+    myTruck.printOrders();
+    assert.deepEqual(myTruck.getAllOrders(), {
+        'me@goldfinder.com': {
+            'coffee': 'double mocha',
+            'emailAddress': 'me@goldfinder.com'
+        }
+    });
 });
